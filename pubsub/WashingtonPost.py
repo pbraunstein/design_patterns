7d6f681b66aebc62f4cbfeaf702ce95a5bd42d1a
pbraunstein <pbraunstein12@gmail.com>
2016-10-25 00:08:28 -0400

    First observer

---
+++ pubsub/WashingtonPost.py
@@ -0,0 +1,25 @@
+from pubsub.observer import Observer
+
+
+class WashingtonPost(Observer):
+    def __init__(self):
+        self.publisher = None
+        self.publisher_name = None
+        self.name = "Washington Post"
+
+    def set_publisher(self, publisher):
+        self.publisher = publisher
+        self.publisher.add_observer(self)
+        self.publisher_name = publisher.get_name()
+
+    def update(self, motto):
+        print "%s reports that %s said %s" % (
+            self.name,
+            self.publisher_name,
+            motto
+        )
+
+    def unfollow(self):
+        self.publisher.remove_observer(self)
+        self.publisher = None
+        self.publisher_name = None
